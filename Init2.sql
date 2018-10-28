7ba2cd9d9e600103725efe108945f7a9fce70f0a
AngelBeats013 <cheng.weisen013@gmail.com>
2018-10-28 15:57:04 -0500

    update mysql setup in init2.sql

--- Init2.sql
+++ Init2.sql
@@ -13,3 +13,14 @@ CREATE TABLE `ATTENDANCE_KEY` (
   CONSTRAINT `fk_attendance_key_courses_id` FOREIGN KEY (`course_id`) REFERENCES `COURSES` (`id`),
   CONSTRAINT `fk_attendance_key_teacher` FOREIGN KEY (`teacher_id`) REFERENCES `USERS` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS syllabus;
+CREATE TABLE `syllabus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ta_name` varchar(30) DEFAULT NULL,
+  `ta_email` varchar(30) DEFAULT NULL,
+  `description` tinytext,
+  `grading` tinytext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
