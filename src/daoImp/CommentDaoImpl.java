b958e096a2359fb7ac84ec189a32a1cb41713364
LIU <liuyf211@gmail.com>
2018-11-18 16:00:14 -0600

    add comments support for teacher

--- src/daoImp/CommentDaoImpl.java
+++ src/daoImp/CommentDaoImpl.java
@@ -18,6 +18,7 @@ public class CommentDaoImpl implements CommentDao {
     public List<Comment> getAll(String userId) {
         List<Comment> results = new ArrayList<>();
         String sql = "SELECT C.*, course.* FROM COMMENTS C, COURSES course, ( select stu.course_id from STU_COURSES stu where stu.stu_id = ?) RES WHERE RES.course_id=c.course_id and RES.course_id=course.id";
+        String sql2 = "SELECT C.*, course.* FROM COMMENTS C, COURSES course WHERE course.teacher_id=? and c.course_id=course.id";
 
         try {
             conn = db.getConnection();
@@ -57,6 +58,46 @@ public class CommentDaoImpl implements CommentDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        try {
+            conn = db.getConnection();
+            ps = conn.prepareStatement(sql2);
+            ps.setString(1, userId);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                Comment c = new Comment();
+                c.setId(resultSet.getString(1));
+                c.setRating(resultSet.getInt(2));
+                c.setContent(resultSet.getString(3));
+                c.setUserId(resultSet.getString(5));
+
+                Course course = new Course();
+                course.setCid(resultSet.getInt(6));
+                course.setCname(resultSet.getString(7));
+                course.setPrefix(resultSet.getString(8));
+                course.setCno(resultSet.getString(9));
+                course.setSno(resultSet.getString(10));
+                course.setRoom(resultSet.getString(11));
+                course.setCapacity(resultSet.getInt(12));
+                course.setYear(resultSet.getString(13));
+                course.setSemester(resultSet.getString(14));
+                course.setSday(resultSet.getString(15));
+                course.setEday(resultSet.getString(16));
+                course.setDays(resultSet.getString(17));
+                course.setStime(resultSet.getString(18));
+                course.setEtime(resultSet.getString(19));
+                course.setTeacher_id(resultSet.getString(20));
+                course.setSyllabus_no(resultSet.getString(21));
+
+                c.setCourse(course);
+
+                results.add(c);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return results;
     }
 
